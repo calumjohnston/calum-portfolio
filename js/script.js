e13d21d31ceacca77784b887a36c33591b5a9467
Calum Johnston <calumjohnston2@gmail.com>
2017-09-02 13:58:46 +1000

    css

--- js/script.js
+++ js/script.js
@@ -6,21 +6,3 @@ jQuery.extend(jQuery.easing,{
         return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
     }
 });
-
-$(document).ready(function(){
-
-    $('a[href=#]').click(function(e){
-        e.preventDefault();
-        $('nav').removeClass('visible');
-        $('html,body').stop().animate({scrollTop: $('.'+$(this).data('scrollto')).offset().top-65 }, 700, 'easeInOutExpo', function(){});
-    });
-
-    $('.toggle-menu').click(function(){
-        $('nav').toggleClass('visible');
-    });
-
-    if($(window).width() < 800){
-        $('.pricing > div > div:nth-of-type(3)').insertAfter($('.pricing > div > div:nth-of-type(1)'));
-    }
-    
-});
